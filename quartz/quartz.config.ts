b6ad8cfe719b0bf481a24ff1813835295b291397
Arthur Coelho <arthur.cavalcante.puc@gmail.com>
2026-05-01 21:09:43 -0300

    quartz: brain emoji + terminal-style title glow + white current-node in graph

--- quartz/quartz.config.ts
+++ quartz/quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Arthur's Brain",
+    pageTitle: "🧠 Arthur's Brain",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
